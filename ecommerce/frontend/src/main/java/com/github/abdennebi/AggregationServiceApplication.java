41ef7164dca318457639b8e59d73e546009ca4ab
Abdennebi Mohamed <abdennebi@gmail.com>
2017-05-21 10:37:17 +0200

    Remove client discovery

--- ecommerce/frontend/src/main/java/com/github/abdennebi/AggregationServiceApplication.java
+++ ecommerce/frontend/src/main/java/com/github/abdennebi/AggregationServiceApplication.java
@@ -4,11 +4,9 @@ import com.github.abdennebi.client.ProductClientApi;
 import com.github.abdennebi.client.StockClientApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients(clients = {ProductClientApi.class, StockClientApi.class})
 public class AggregationServiceApplication {
 
--- ecommerce/product-service/src/main/java/com/github/abdennebi/ProductServiceApplication.java
+++ ecommerce/product-service/src/main/java/com/github/abdennebi/ProductServiceApplication.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableJpaRepositories(basePackageClasses = {com.github.abdennebi.domain.ProductRepository.class})
 public class ProductServiceApplication {
 
