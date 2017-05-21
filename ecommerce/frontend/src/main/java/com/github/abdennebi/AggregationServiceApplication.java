bcc854e71f679e5f328709968477d773cd3385f8
Abdennebi Mohamed <abdennebi@gmail.com>
2017-05-21 11:26:42 +0200

    Add swagger support

--- ecommerce/frontend/src/main/java/com/github/abdennebi/AggregationServiceApplication.java
+++ ecommerce/frontend/src/main/java/com/github/abdennebi/AggregationServiceApplication.java
@@ -5,8 +5,10 @@ import com.github.abdennebi.client.StockClientApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableFeignClients(clients = {ProductClientApi.class, StockClientApi.class})
 public class AggregationServiceApplication {
 
--- ecommerce/product-service/src/main/java/com/github/abdennebi/ProductServiceApplication.java
+++ ecommerce/product-service/src/main/java/com/github/abdennebi/ProductServiceApplication.java
@@ -8,8 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @EnableJpaRepositories(basePackageClasses = {com.github.abdennebi.domain.ProductRepository.class})
 public class ProductServiceApplication {
 
