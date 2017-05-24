5180283c403b69da579fc7890e4e23a58abcf557
Mohamed ABDENNEBI <abdennebi@gmail.com>
2017-05-24 17:08:48 +0200

    Parameterize product and stock URL

--- online-store/frontend/src/main/java/com/github/abdennebi/config/ClientConfig.java
+++ online-store/frontend/src/main/java/com/github/abdennebi/config/ClientConfig.java
@@ -5,18 +5,25 @@ import com.github.abdennebi.client.StockClientApi;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClientConfig {
 
+    @Value("${frontend.product.url}")
+    String productUrl;
+
+    @Value("${frontend.stock.url}")
+    String stockUrl;
+
     @Bean
     public ProductClientApi productClientApi() {
         return Feign.builder()
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
-                .target(ProductClientApi.class, "http://localhost:52001");
+                .target(ProductClientApi.class, productUrl);
     }
 
     @Bean
@@ -24,6 +31,6 @@ public class ClientConfig {
         return Feign.builder()
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
-                .target(StockClientApi.class, "http://localhost:52002");
+                .target(StockClientApi.class, stockUrl);
     }
 }
