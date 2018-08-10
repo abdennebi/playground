5b0001faeb3b30afe130f06a4ea02775f393b96c
Abdennebi Mohamed <abdennebi@gmail.com>
2018-08-10 12:27:34 +0200

    Upgrade to Spring Boot 2

--- online-store/product-service/src/main/java/com/github/abdennebi/controller/ProductController.java
+++ online-store/product-service/src/main/java/com/github/abdennebi/controller/ProductController.java
@@ -5,12 +5,14 @@ import com.github.abdennebi.domain.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -28,7 +30,11 @@ public class ProductController {
 
     @GetMapping("/{id}")
     ResponseEntity<Product> findById(@PathVariable Long id) {
-        return ResponseEntity.ok().body(productRepository.findOne(id));
+        Product example = new Product();
+        example.setId(id);
+        Optional<Product> result = productRepository.findOne(Example.of(example));
+
+        return ResponseEntity.ok().body(result.get());
     }
 
     @GetMapping("/ref/{ref}")
