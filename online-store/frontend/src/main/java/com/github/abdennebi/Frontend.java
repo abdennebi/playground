07f97e5d1a7082ac3b67ed00ad80aad1949be5d0
Abdennebi Mohamed <abdennebi@gmail.com>
2018-08-10 12:27:06 +0200

    Externalize SpringFox config

--- online-store/frontend/src/main/java/com/github/abdennebi/Frontend.java
+++ online-store/frontend/src/main/java/com/github/abdennebi/Frontend.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-
 public class Frontend {
 
     public static void main(String[] args) {
---
+++ online-store/frontend/src/main/java/com/github/abdennebi/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.github.abdennebi.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Online Store API", "Online Store API", "1.0.0", "", new Contact("", "", ""), "", "", Collections.emptyList());
+    }
+}
