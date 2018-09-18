45c94eacd4565ec292c713e51bffcfa4fdc6256a
Abdennebi Mohamed <abdennebi@gmail.com>
2018-09-18 20:49:14 +0200

    Fix #22. Move infra code to a dedicated project https://github.com/CloudNativeStack/playground-infrastructure (#23)

--- infrastructure/gcp/terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-// Configures the Google Cloud Provider with default settings
-provider "google" {
-  project = "${var.project}"
-  version = "~> 1.13"
-}
--- infrastructure/gcp/terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "zone" {
-  description = "The zone in which to create the Kubernetes cluster. Must match the region"
-  type        = "string"
-}
-
-variable "project" {
-  description = "The name of the project in which to create the Kubernetes cluster."
-  type        = "string"
-}
